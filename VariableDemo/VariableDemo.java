45d47859fc9850fb65ad53256f52902c7d37f346
gskundi87 <gskundi87@gmail.com>
2021-07-15 12:22:24 -0700

    java

---
+++ VariableDemo/VariableDemo.java
@@ -0,0 +1,22 @@
+public class VariableDemo
+{
+    public static void main(String args[])
+    {
+        int width = 10;
+        System.out.print("width: ");
+        System.out.println(width);
+
+        width =20;
+        System.out.print("width: ");
+        System.out.println(width);
+
+        int height = 20;
+        width = height + 10;
+        System.out.print("width: ");
+        System.out.println(width);
+
+        width = width + 10;
+        System.out.print("width: ");
+        System.out.println(width);
+    }
+}
